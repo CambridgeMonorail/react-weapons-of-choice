819d1a1cbf06912d69f0a58213b24576e0a6b1c2
Tim Morris <TimDMorris@users.noreply.github.com>
2024-11-23 14:37:44 +0000

    Feat/add all shadcn components (#3)
    * Update Tailwind CSS paths and adjust TypeScript configuration for shared library
    
    * Add new UI components and hooks for shared library, update utils path

---
+++ libs/shared/src/components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@react-monorepo/shared/lib/utils"
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
