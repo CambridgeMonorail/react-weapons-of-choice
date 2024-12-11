32a28957883e4cc030154cdbc214cbbf9cf46a08
Tim Morris <TimDMorris@users.noreply.github.com>
2024-12-11 17:18:18 +0000

    Add landing and shadcnui-blocks libraries with initial setup and components

---
+++ libs/landing/src/lib/landing.tsx
@@ -0,0 +1,14 @@
+import styled from 'tailwind';
+
+const StyledLanding = styled.div`
+  color: pink;
+`;
+export function Landing() {
+  return (
+    <StyledLanding>
+      <h1>Welcome to Landing!</h1>
+    </StyledLanding>
+  );
+}
+
+export default Landing;
---
+++ libs/shadcnui-blocks/src/lib/shadcnui-blocks.tsx
@@ -0,0 +1,14 @@
+import styled from 'tailwind';
+
+const StyledShadcnuiBlocks = styled.div`
+  color: pink;
+`;
+export function ShadcnuiBlocks() {
+  return (
+    <StyledShadcnuiBlocks>
+      <h1>Welcome to ShadcnuiBlocks!</h1>
+    </StyledShadcnuiBlocks>
+  );
+}
+
+export default ShadcnuiBlocks;
