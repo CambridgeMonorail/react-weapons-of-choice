77eae32b08407e7795a9e21eca59904dc8015115
Tim Morris <TimDMorris@users.noreply.github.com>
2024-12-16 09:24:43 +0000

    refactor(storybook): streamline story sorting configuration

--- libs/storybook-host/.storybook/preview.ts
+++ libs/storybook-host/.storybook/preview.ts
@@ -3,16 +3,17 @@ import '../../common-tailwind/src/main.css';
 export const parameters = {
   options: {
     initialActive: 'docs',
+    storySort: {
+      order: [
+        'Introduction',
+        'Getting Started',
+        'Best Practices',
+        'Resources',
+      ],
+    },
   },
   docs: {
     page: null,
   },
-  storySort: {
-    order: [
-      'Introduction',
-      'Getting Started',
-      'Best Practices',
-      'Resources',
-    ],
-  },
+  
 };
