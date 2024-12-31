4875e2c7f774d425945e20b9ccccb3bca2d3e076
Tim Morris <TimDMorris@users.noreply.github.com>
2024-12-31 12:46:51 +0000

    docs(landing): update documentation for HeroSection and AboutSection components

--- libs/landing/src/lib/components/index.ts
+++ libs/landing/src/lib/components/index.ts
@@ -3,6 +3,6 @@ export * from './community-section/CommunitySection';
 export * from './cta-section/CTASection';
 export * from './demo-section/DemoSection';
 export * from './features-section';
-export * from './hero-section/HeroSection';
 export * from './footer-section/Footer';
 export * from './get-started-section/GetStartedSection';
+export * from './hero-section/HeroSection';
